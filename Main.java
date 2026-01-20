af39eabf16e89a84ae96f9c6d3500e65b1bcec73
devnadifa <devnadifa@gmail.com>
2026-01-20 10:37:40 +0300

    simple banking system

--- Main.java
+++ Main.java
@@ -1,78 +1,68 @@
 import java.util.Scanner;
+
 public class Main {
-  static Scanner scanner = new Scanner(System.in);
-  public static void main (String[] args) {
-
-    //STEPS FOR SIMPLE BACKING PROGRAM
-
-    //DECLARE VARIABLES
+  static Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
     double balance = 0;
     boolean isRunnig = true;
     int choice;
-    
+
     while(isRunnig) {
-    //DISPLAY MENU
-      System.out.println("************");
-      System.out.println("BANKING PROGRAM");
-      System.out.println("************");
+      System.out.println("*******************");
       System.out.println("1. Show Balance");
       System.out.println("2. Deposit");
       System.out.println("3. Withdraw");
       System.out.println("4. Exit");
-      System.out.println("************");
+      System.out.println("*******************");
 
-      //GET AND PROCESS USERS CHOICE
-      System.out.print("Enter your choice (1 - 4): ");
-      choice = scanner.nextInt();
-
+      System.out.print("Enter your choice(1 - 4): ");
+      choice = input.nextInt();
+     
       switch (choice) {
         case 1 -> showBalance(balance);
         case 2 -> balance += deposit();
         case 3 -> balance -= withdraw(balance);
-        case 4 -> isRunnig = false ;  
-        default -> System.out.println("INVALID CHOICE");
-
-      }
+        case 4 -> isRunnig = false;
+        default -> System.out.println("invalid choice");
+    
+      }      
     }
+    System.out.println("*******************");
+    System.out.println("Thank you for using our banking system");
+    System.out.println("*******************");
 
-    //EXIT MESSAGE
-    System.out.println("*****************");
-    System.out.println("Thank you have a nice day!");
-    System.out.println("*****************");
-
-    scanner.close();
-
+    input.close();
   }
-  //showBalance()
+  //show balance
   static void showBalance(double balance) {
-    System.out.printf("Ksh%.2f\n", balance);
+    System.out.printf("Your balance is Kshs%.2f \n", balance);
   }
-  //deposit()
+  // deposit
   static double deposit() {
-    double amount;
-    System.out.println("Enter amount to be deposited: ");
-    amount = scanner.nextDouble();
+    System.out.println("Enter amount to deposit: ");
+    double amount = input.nextDouble();
     if (amount < 0) {
-      System.out.println("Amount cant be negative");
+      System.out.println("Amount can't be negative!");
       return 0;
     } else {
       return amount;
     }
+
   }
-  
+  //withdraw
   static double withdraw(double balance) {
-    double amount;
-    System.out.print("Enter amount to be withdrawn: ");
-    amount = scanner.nextDouble();
-    if(amount > balance) {
-      System.out.println("Insufficient funds");
+    System.out.println("Enter amount to withdraw: ");
+    double amount = input.nextDouble();
+
+    if(amount < 0) {
+      System.out.println("can't withdraw negative amount");
       return 0;
-    } else if (amount < 0) {
-      System.out.println("Amount cant be negative");
+    } else if (amount > balance) {
+      System.out.println("Insufficient money");
       return 0;
     } else {
-    return amount;
+      return amount;
     }
-  }
 
+  }
 }
